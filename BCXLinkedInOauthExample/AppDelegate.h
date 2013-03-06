35cf2a9479547b452891af99bb8f07b17b81e8cf
Rodrigo Aguilar <rodrigo@rodrigoaguilar.com>
2013-03-06 12:17:50 -0300

    Initial Commit

---
+++ BCXLinkedInOauthExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BCXLinkedInOauthExample
+//
+//  Created by Rodrigo Aguilar on 3/6/13.
+//  Copyright (c) 2013 bNapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BCXLinkedInOauthExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BCXLinkedInOauthExample
+//
+//  Created by Rodrigo Aguilar on 3/6/13.
+//  Copyright (c) 2013 bNapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
